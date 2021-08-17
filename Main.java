2b9ec320ac804b9291f98bf0da30f7354e678abf
vaish04g <vaishnavigadage@gmail.com>
2021-08-17 13:44:27 +0530

    sacchii huaa

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
 		
 		User u1 = new User("Rai",12345,6969,100000.1);
 		u1.deposit_amount(2.5);
-		
+		System.out.println("Just to be sure i nailed the push xP");
 		u1.toString();
 	}
 
