83c983395ec360139d288faa1aa2fa0d7ad90d6d
vaish04g <vaishnavigadage@gmail.com>
2021-08-18 19:12:26 +0530

    Tried Login validity

--- client/Final_interface.java
+++ client/Final_interface.java
@@ -10,29 +10,35 @@ import java.util.ArrayList;
 
 public class Final_interface {
     public static void main(String[] args) throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
-
-        Login l = new Login();
-        ATM_Interface a = new ATM_Interface();
         try {
             UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
         } finally {
-
         }
 
-        l.setContentPane(new Login().LoginMain);
+        ArrayList<User> A1 = new ArrayList<User>();
+        User u1 = new User("Rai",12345,6969,100000.1);
+        User u2 = new User("Sai",24680,6968,200000.1);
+        User u3 = new User("Mai",13579,6967,300000.1);
+        A1.add(u1);
+        A1.add(u2);
+        A1.add(u3);
+
+        Login l = new Login(A1);
+        ATM_Interface a = new ATM_Interface();
+
+        l.setContentPane(new Login(A1).LoginMain);
         l.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         l.setVisible(true);
         l.pack();
-
 
         a.setContentPane(new ATM_Interface().Main);
         a.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         a.setVisible(true);
         a.pack();
 
-        ArrayList<User> A1 = new ArrayList<User>();
-        User u = new User("Rai",12345,6969,100000.1);
-        A1.add(u);
+
+
+
 
     }
 }
--- logic/Login.java
+++ logic/Login.java
@@ -3,21 +3,76 @@ package logic;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class Login extends JFrame{
     public JPanel LoginMain;
-    private JPasswordField passwordField1;
-    private JTextField textField1;
     private JButton ENTERButton;
+    private JPasswordField passwordField1;
+    private JTextArea textArea1;
 
-    public Login() {
+      ArrayList<User> sampleUsers = new ArrayList<User>();
+//    User u1 = new User("Rai",12345,6969,100000.1);
+//    User u2 = new User("Sai",24680,6968,200000.1);
+//    User u3 = new User("Mai",13579,6967,300000.1);
+
+
+
+
+    public Login(ArrayList Users) {
+
+        this.sampleUsers = Users;
         ENTERButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                boolean validCard = cardValidity(textArea1.getText());
+                System.out.println(textArea1.getText() + validCard);
+                if (validCard != true)
+                {
+                    JOptionPane.showMessageDialog(null,"INVALID ACCOUNT NUMBER");
+                }
+                else
+                {
+                    boolean validPin = pinValidity(passwordField1.getPassword());
+                    if(validPin != true)
+                    {
+                        JOptionPane.showMessageDialog(null,"INVALID PIN");
+                    }
+                    else
+                    {
+                        JOptionPane.showMessageDialog(null,"LOGGED IN SUCESSFULLY!");
+                    }
+                }
             }
         });
 
+    }
 
+    public boolean cardValidity(String number){
+        boolean valid = false;
+        for (User u:sampleUsers) {
+//            System.out.println("In");
+//            System.out.println(u.getCard_number());
+//            System.out.println(Integer.parseInt(number));
+            if (u.getCard_number() == Integer.parseInt(number)) {
+//                System.out.println("In true");
+                valid = true;
+                break;
+            }
+        }
+//        System.out.println(valid + "!");
+        return valid;
+    }
+
+    public boolean pinValidity(char[] pin){
+        boolean valid = false;
+        for (User u:sampleUsers) {
+            System.out.println(u.getPin() + Integer.parseInt(String.valueOf(pin)));
+            if (u.getPin() == Integer.parseInt(String.valueOf(pin))) {
+                valid = true;
+                break;
+            }
+        }
+        return valid;
     }
 }
