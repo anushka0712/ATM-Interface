e8139741d10cabf593d3fe5178ddd2a09b584af2
vaish04g <vaishnavigadage@gmail.com>
2021-08-19 10:58:56 +0530

    Tried Login validity

--- logic/Login.java
+++ logic/Login.java
@@ -5,18 +5,16 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-public class Login extends JFrame{
+public class Login extends JFrame {
     public JPanel LoginMain;
     private JButton ENTERButton;
     private JPasswordField passwordField1;
     private JTextArea textArea1;
 
-      ArrayList<User> sampleUsers = new ArrayList<User>();
+    ArrayList<User> sampleUsers = new ArrayList<User>();
 //    User u1 = new User("Rai",12345,6969,100000.1);
 //    User u2 = new User("Sai",24680,6968,200000.1);
 //    User u3 = new User("Mai",13579,6967,300000.1);
-
-
 
 
     public Login(ArrayList Users) {
@@ -27,20 +25,14 @@ public class Login extends JFrame{
             public void actionPerformed(ActionEvent e) {
                 boolean validCard = cardValidity(textArea1.getText());
                 System.out.println(textArea1.getText() + validCard);
-                if (validCard != true)
-                {
-                    JOptionPane.showMessageDialog(null,"INVALID ACCOUNT NUMBER");
-                }
-                else
-                {
+                if (validCard != true) {
+                    JOptionPane.showMessageDialog(null, "INVALID ACCOUNT NUMBER");
+                } else {
                     boolean validPin = pinValidity(passwordField1.getPassword());
-                    if(validPin != true)
-                    {
-                        JOptionPane.showMessageDialog(null,"INVALID PIN");
-                    }
-                    else
-                    {
-                        JOptionPane.showMessageDialog(null,"LOGGED IN SUCESSFULLY!");
+                    if (validPin != true) {
+                        JOptionPane.showMessageDialog(null, "INVALID PIN");
+                    } else {
+                        JOptionPane.showMessageDialog(null, "LOGGED IN SUCESSFULLY!");
                     }
                 }
             }
@@ -48,9 +40,9 @@ public class Login extends JFrame{
 
     }
 
-    public boolean cardValidity(String number){
+    public boolean cardValidity(String number) {
         boolean valid = false;
-        for (User u:sampleUsers) {
+        for (User u : sampleUsers) {
 //            System.out.println("In");
 //            System.out.println(u.getCard_number());
 //            System.out.println(Integer.parseInt(number));
@@ -64,9 +56,9 @@ public class Login extends JFrame{
         return valid;
     }
 
-    public boolean pinValidity(char[] pin){
+    public boolean pinValidity(char[] pin) {
         boolean valid = false;
-        for (User u:sampleUsers) {
+        for (User u : sampleUsers) {
             System.out.println(u.getPin() + Integer.parseInt(String.valueOf(pin)));
             if (u.getPin() == Integer.parseInt(String.valueOf(pin))) {
                 valid = true;
